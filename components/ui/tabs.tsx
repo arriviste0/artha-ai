69d2a459b82a59c2a55c65260bc1e4ab959682ad
arriviste0 <121562328+arriviste0@users.noreply.github.com>
2026-06-26 02:55:38 +0530

    fix: hide native scrollbar arrows on TabsList when it overflows

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -26,7 +26,7 @@ function Tabs({
 }
 
 const tabsListVariants = cva(
-  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg p-[3px] text-muted-foreground group-data-[orientation=horizontal]/tabs:h-9 group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col data-[variant=line]:rounded-none overflow-hidden",
+  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg p-[3px] text-muted-foreground group-data-[orientation=horizontal]/tabs:h-9 group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col data-[variant=line]:rounded-none overflow-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]",
   {
     variants: {
       variant: {
